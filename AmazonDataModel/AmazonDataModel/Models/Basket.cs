f8fcaee71974f9412d2366fe0627798bdecebaad
rahib <rsayed@spartaglobal.com>
2019-03-19 09:45:50 +0000

    Created all the Model classes

---
+++ AmazonDataModel/AmazonDataModel/Models/Basket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmazonDataModel.Models
+{
+    public class Basket
+    {
+        private int BasketID;
+        private string[] BasketItems;
+        private Product product;
+
+        public int BasketID1 { get => BasketID; set => BasketID = value; }
+        public string[] BasketItems1 { get => BasketItems; set => BasketItems = value; }
+        public Product Product { get => product; set => product = value; }
+    }
+}
---
+++ AmazonDataModel/AmazonDataModel/Models/HelpDesk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmazonDataModel.Models
+{
+    public class HelpDesk
+    {
+        private int helpDeskID;
+        private string issueMessage;
+
+        public int HelpDeskID { get => helpDeskID; set => helpDeskID = value; }
+        public string IssueMessage { get => issueMessage; set => issueMessage = value; }
+    }
+}
